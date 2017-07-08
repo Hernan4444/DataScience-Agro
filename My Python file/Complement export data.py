08a0e20747a0f17a2a249ba3a42b1cfbf2bf3973
Hernan4444 <hfvaldivieso@uc.cl>
2017-07-08 14:53:47 -0400

    :rocket:

--- My Python file/Complement export data.py
+++ My Python file/Complement export data.py
@@ -2,72 +2,68 @@ import pandas as pd
 import os
 
 
-cwd = os.getcwd()
+columns_file =\
+    ["FECHAACEPT", "NUMEROIDENT", "ADUANA", "TIPOOPERACION",
+     "CODIGORUTEXPORTADORPPAL", "NRO_EXPORTADOR",
+     "PORCENTAJEEXPPPAL", "COMUNAEXPORTADORPPAL", "CODIGORUTEXPSEC",
+     "NRO_EXPORTADOR_SEC", "PORCENTAJEEXPSECUNDARIO",
+     "COMUNAEXPSECUNDARIO", "PUERTOEMB", "GLOSAPUERTOEMB",
+     "REGIONORIGEN", "TIPOCARGA", "VIATRANSPORTE", "PUERTODESEMB",
+     "GLOSAPUERTODESEMB", "PAISDESTINO", "GLOSAPAISDESTINO",
+     "NOMBRECIATRANSP", "PAISCIATRANSP", "RUTCIATRANSP",
+     "DVRUTCIATRANSP", "NOMBREEMISORDOCTRANSP", "RUTEMISOR",
+     "DVRUTEMISOR", "CODIGOTIPOAUTORIZA", "NUMEROINFORMEEXPO",
+     "DVNUMEROINFORMEEXP", "FECHAINFORMEEXP", "MONEDA",
+     "MODALIDADVENTA", "CLAUSULAVENTA", "FORMAPAGO",
+     "VALORCLAUSULAVENTA", "COMISIONESEXTERIOR", "OTROSGASTOS",
+     "VALORLIQUIDORETORNO", "NUMEROREGSUSP", "ADUANAREGSUSP",
+     "PLAZOVIGENCIAREGSUSP", "TOTALITEM", "TOTALBULTOS",
+     "PESOBRUTOTOTAL", "TOTALVALORFOB", "VALORFLETE", "CODIGOFLETE",
+     "VALORSEGURO", "CODIGOSEG", "VALORCIF", "NUMEROPARCIALIDAD",
+     "TOTALPARCIALES", "PARCIAL", "OBSERVACION",
+     "NUMERODOCTOCANCELA", "FECHADOCTOCANCELA", "TIPODOCTOCANCELA",
+     "PESOBRUTOCANCELA", "TOTALBULTOSCANCELA", "NUMEROITEM",
+     "NOMBRE", "ATRIBUTO1", "ATRIBUTO2", "ATRIBUTO3", "ATRIBUTO4",
+     "ATRIBUTO5", "ATRIBUTO6", "CODIGOARANCEL", "UNIDADMEDIDA",
+     "CANTIDADMERCANCIA", "FOBUNITARIO", "FOBUS",
+     "CODIGOOBSERVACION1", "VALOROBSERVACION1", "GLOSAOBSERVACION1",
+     "CODIGOOBSERVACION2", "VALOROBSERVACION2", "GLOSAOBSERVACION2",
+     "CODIGOOBSERVACION3", "VALOROBSERVACION3", "GLOSAOBSERVACION3",
+     "PESOBRUTOITEM"]
 
-read_path = cwd + os.sep + "Original_Data" + os.sep + "export_complement"
-complement_path = cwd + os.sep + "Filter_Data" + os.sep + "export"
+columns_package = \
+    ["NUMEROIDENT", "FECHAACEPTPACKAGE", "NUMEROBULTO",
+     "TIPOBULTO", "CANTIDADBULTO", "IDENTIFICACIONBULTO"]
+
+columns_transport = \
+    ["NUMEROIDENT", "FECHAACEPTTRANSPORT", "NSECDOCTRANSP",
+     "NUMERODOCTRANSP", "FECHADOCTRANSP", "NOMBRENAVE",
+     "NUMEROVIAJE"]
 
 file_to_complement = {
     "2017": ["01 - Enero"]
 }
 
-columns_bultos = ["NUMEROIDENT", "FECHAACEPTPACKAGE", "NUMEROBULTO", "TIPOBULTO",
-                 "CANTIDADBULTO", "IDENTIFICACIONBULTO"]
 
-columns_transporte = [ "NUMEROIDENT", "FECHAACEPTTRANSPORT", "NSECDOCTRANSP",
-                       "NUMERODOCTRANSP", "FECHADOCTRANSP", "NOMBRENAVE",
-                       "NUMEROVIAJE"]
+cwd = os.getcwd()
 
-columns_file = [ "FECHAACEPT", "NUMEROIDENT", "ADUANA", "TIPOOPERACION",
-                 "CODIGORUTEXPORTADORPPAL", "NRO_EXPORTADOR",
-                 "PORCENTAJEEXPPPAL", "COMUNAEXPORTADORPPAL", "CODIGORUTEXPSEC",
-                 "NRO_EXPORTADOR_SEC", "PORCENTAJEEXPSECUNDARIO",
-                 "COMUNAEXPSECUNDARIO", "PUERTOEMB", "GLOSAPUERTOEMB",
-                 "REGIONORIGEN", "TIPOCARGA", "VIATRANSPORTE", "PUERTODESEMB",
-                 "GLOSAPUERTODESEMB", "PAISDESTINO", "GLOSAPAISDESTINO",
-                 "NOMBRECIATRANSP", "PAISCIATRANSP", "RUTCIATRANSP",
-                 "DVRUTCIATRANSP", "NOMBREEMISORDOCTRANSP", "RUTEMISOR",
-                 "DVRUTEMISOR", "CODIGOTIPOAUTORIZA", "NUMEROINFORMEEXPO",
-                 "DVNUMEROINFORMEEXP", "FECHAINFORMEEXP", "MONEDA",
-                 "MODALIDADVENTA", "CLAUSULAVENTA", "FORMAPAGO",
-                 "VALORCLAUSULAVENTA", "COMISIONESEXTERIOR", "OTROSGASTOS",
-                 "VALORLIQUIDORETORNO", "NUMEROREGSUSP", "ADUANAREGSUSP",
-                 "PLAZOVIGENCIAREGSUSP", "TOTALITEM", "TOTALBULTOS",
-                 "PESOBRUTOTOTAL", "TOTALVALORFOB", "VALORFLETE", "CODIGOFLETE",
-                 "VALORSEGURO", "CODIGOSEG", "VALORCIF", "NUMEROPARCIALIDAD",
-                 "TOTALPARCIALES", "PARCIAL", "OBSERVACION",
-                 "NUMERODOCTOCANCELA", "FECHADOCTOCANCELA", "TIPODOCTOCANCELA",
-                 "PESOBRUTOCANCELA", "TOTALBULTOSCANCELA", "NUMEROITEM",
-                 "NOMBRE", "ATRIBUTO1", "ATRIBUTO2", "ATRIBUTO3", "ATRIBUTO4",
-                 "ATRIBUTO5", "ATRIBUTO6", "CODIGOARANCEL", "UNIDADMEDIDA",
-                 "CANTIDADMERCANCIA", "FOBUNITARIO", "FOBUS",
-                 "CODIGOOBSERVACION1", "VALOROBSERVACION1", "GLOSAOBSERVACION1",
-                 "CODIGOOBSERVACION2", "VALOROBSERVACION2", "GLOSAOBSERVACION2",
-                 "CODIGOOBSERVACION3", "VALOROBSERVACION3", "GLOSAOBSERVACION3",
-                 "PESOBRUTOITEM"]
-
-
-['FECHAACEPT', 'NUMEROIDENT', 'ADUANA', 'TIPOOPERACION', 'CODIGORUTEXPORTADORPPAL',
- 'NRO_EXPORTADOR', 'PORCENTAJEEXPPPAL', 'COMUNAEXPORTADORPPAL', 'CODIGORUTEXPSEC', 'NRO_EXPORTADOR_SEC', 'PORCENTAJEEXPSECUNDARIO', 'COMUNAEXPSECUNDARIO', 'PUERTOEMB', 'GLOSAPUERTOEMB', 'REGIONORIGEN', 'TIPOCARGA', 'VIATRANSPORTE', 'PUERTODESEMB', 'GLOSAPUERTODESEMB', 'PAISDESTINO', 'GLOSAPAISDESTINO', 'NOMBRECIATRANSP', 'PAISCIATRANSP', 'RUTCIATRANSP', 'DVRUTCIATRANSP', 'NOMBREEMISORDOCTRANSP', 'RUTEMISOR', 'DVRUTEMISOR', 'CODIGOTIPOAUTORIZA', 'NUMEROINFORMEEXPO', 'DVNUMEROINFORMEEXP', 'FECHAINFORMEEXP', 'MONEDA', 'MODALIDADVENTA', 'CLAUSULAVENTA', 'FORMAPAGO', 'VALORCLAUSULAVENTA', 'COMISIONESEXTERIOR', 'OTROSGASTOS', 'VALORLIQUIDORETORNO', 'NUMEROREGSUSP', 'ADUANAREGSUSP', 'PLAZOVIGENCIAREGSUSP', 'TOTALITEM', 'TOTALBULTOS', 'PESOBRUTOTOTAL', 'TOTALVALORFOB', 'VALORFLETE', 'CODIGOFLETE', 'VALORSEGURO', 'CODIGOSEG', 'VALORCIF', 'NUMEROPARCIALIDAD', 'TOTALPARCIALES', 'PARCIAL', 'OBSERVACION', 'NUMERODOCTOCANCELA', 'FECHADOCTOCANCELA', 'TIPODOCTOCANCELA', 'PESOBRUTOCANCELA', 'TOTALBULTOSCANCELA', 'NUMEROITEM', 'NOMBRE', 'ATRIBUTO1', 'ATRIBUTO2', 'ATRIBUTO3', 'ATRIBUTO4', 'ATRIBUTO5', 'ATRIBUTO6', 'CODIGOARANCEL', 'UNIDADMEDIDA', 'CANTIDADMERCANCIA', 'FOBUNITARIO', 'FOBUS', 'CODIGOOBSERVACION1', 'VALOROBSERVACION1', 'GLOSAOBSERVACION1', 'CODIGOOBSERVACION2', 'VALOROBSERVACION2', 'GLOSAOBSERVACION2', 'CODIGOOBSERVACION3', 'VALOROBSERVACION3', 'GLOSAOBSERVACION3', 'PESOBRUTOITEM',
-
- 'FECHAACEPT_x', 'NUMEROBULTO', 'TIPOBULTO', 'CANTIDADBULTO', 'IDENTIFICACIONBULTO',
- 'FECHAACEPT_y', 'NSECDOCTRANSP', 'NUMERODOCTRANSP', 'FECHADOCTRANSP', 'NOMBRENAVE', 'NUMEROVIAJE']
-
+read_path = cwd + os.sep + "Original_Data" + os.sep + "export_complement"
+complement_path = cwd + os.sep + "Filter_Data" + os.sep + "export"
 
 for year, files in file_to_complement.items():
     for file in files:
         main_file = file + ".txt"
-        bultos_file = file + " Bultos.txt"
-        transporte_file = file + " Transporte.txt"
+        packages_file = file + " Bultos.txt"
+        transport_file = file + " Transporte.txt"
 
-        bultos_filename = read_path + os.sep + year + os.sep + bultos_file
-        transporte_filename = read_path + os.sep + year + os.sep + transporte_file
+        packages_filename = read_path + os.sep + year + os.sep + packages_file
+        transport_filename = read_path + os.sep + year + os.sep + transport_file
         main_filename = complement_path + os.sep + year + os.sep + main_file
 
-        bultos = pd.read_csv(bultos_filename, names=columns_bultos, header=None,
-                             sep=";", encoding="latin1")
+        bultos = pd.read_csv(packages_filename, names=columns_package,
+                             header=None, sep=";", encoding="latin1")
 
-        transporte = pd.read_csv(transporte_filename, names=columns_transporte,
+        transporte = pd.read_csv(transport_filename, names=columns_transport,
                                  header=None, sep=";", encoding="latin1")
 
         file = pd.read_csv(main_filename, names=columns_file, header=None,
@@ -77,11 +73,7 @@ for year, files in file_to_complement.items():
                          pd.merge(bultos, transporte, on="NUMEROIDENT"),
                          on="NUMEROIDENT")
 
-        final.to_csv(main_filename, sep=";", encoding="latin1", header=None, index=False)
+        final.to_csv(main_filename, sep=";", encoding="latin1", header=None,
+                     index=False)
 
-
-print("Export filtered")
-
-
-
-
+print("Complement Complete")
--- My Python file/Extract_import_data.py
+++ My Python file/Extract_import_data.py
@@ -1,23 +1,21 @@
 import os
-import pandas
-from filter_file import filter_file
+import pandas as pd
+from filter_file import filter_import_file
 
-cwd = os.getcwd()
-chunksize = 10 ** 3
 
 columns =\
     ["TPO_DOCTO", "ADU", "FORM", "FECVENCI", "CODCOMUN", "NUM_UNICO_IMPORTADOR",
-     "CODPAISCON", "DESDIRALM", "CODCOMRS", "ADUCTROL", "NUMPLAZO", "INDPARCIAL",
-     "NUMHOJINS", "TOTINSUM", "CODALMA", "NUM_RS", "FEC_RS", "ADUA_RS",
-     "NUMHOJANE", "NUM_SEC", "PA_ORIG", "PA_ADQ", "VIA_TRAN", "TRANSB",
-     "PTO_EMB", "PTO_DESEM", "TPO_CARGA", "ALMACEN", "FEC_ALMAC", "FECRETIRO",
-     "NU_REGR", "ANO_REG", "CODVISBUEN", "NUMREGLA", "NUMANORES", "CODULTVB",
-     "PAGO_GRAV", "FECTRA", "FECACEP", "GNOM_CIA_T", "CODPAISCIA", "NUMRUTCIA",
-     "DIGVERCIA", "NUM_MANIF", "NUM_MANIF1", "NUM_MANIF2", "FEC_MANIF",
-     "NUM_CONOC", "FEC_CONOC", "NOMEMISOR", "NUMRUTEMI", "DIGVEREMI",
-     "GREG_IMP", "REG_IMP", "BCO_COM", "CODORDIV", "FORM_PAGO", "NUMDIAS",
-     "VALEXFAB", "MONEDA", "MONGASFOB", "CL_COMPRA", "TOT_ITEMS", "FOB",
-     "TOT_HOJAS", "COD_FLE", "FLETE", "TOT_BULTOS", "COD_SEG", "SEGURO",
+     "CODPAISCON", "DESDIRALM", "CODCOMRS", "ADUCTROL", "NUMPLAZO",
+     "INDPARCIAL", "NUMHOJINS", "TOTINSUM", "CODALMA", "NUM_RS", "FEC_RS",
+     "ADUA_RS", "NUMHOJANE", "NUM_SEC", "PA_ORIG", "PA_ADQ", "VIA_TRAN",
+     "TRANSB", "PTO_EMB", "PTO_DESEM", "TPO_CARGA", "ALMACEN", "FEC_ALMAC",
+     "FECRETIRO", "NU_REGR", "ANO_REG", "CODVISBUEN", "NUMREGLA", "NUMANORES",
+     "CODULTVB", "PAGO_GRAV", "FECTRA", "FECACEP", "GNOM_CIA_T", "CODPAISCIA",
+     "NUMRUTCIA", "DIGVERCIA", "NUM_MANIF", "NUM_MANIF1", "NUM_MANIF2",
+     "FEC_MANIF", "NUM_CONOC", "FEC_CONOC", "NOMEMISOR", "NUMRUTEMI",
+     "DIGVEREMI", "GREG_IMP", "REG_IMP", "BCO_COM", "CODORDIV", "FORM_PAGO",
+     "NUMDIAS", "VALEXFAB", "MONEDA", "MONGASFOB", "CL_COMPRA", "TOT_ITEMS",
+     "FOB", "TOT_HOJAS", "COD_FLE", "FLETE", "TOT_BULTOS", "COD_SEG", "SEGURO",
      "TOT_PESO", "CIF", "NUM_AUT", "FEC_AUT", "GBCOCEN", "ID_BULTOS",
      "TPO_BUL1", "CANT_BUL1", "TPO_BUL2", "CANT_BUL2", "TPO_BUL3", "CANT_BUL3",
      "TPO_BUL4", "CANT_BUL4", "TPO_BUL5", "CANT_BUL5", "TPO_BUL6", "CANT_BUL6",
@@ -28,19 +26,21 @@ columns =\
      "FEC_502", "VAL_602", "FEC_503", "VAL_603", "FEC_504", "VAL_604",
      "FEC_505", "VAL_605", "FEC_506", "VAL_606", "FEC_507", "VAL_607", "TASA",
      "NCUOTAS", "ADU_DI", "NUM_DI", "FEC_DI", "MON_699", "MON_199", "NUMITEM",
-     "DNOMBRE", "DMARCA", "DVARIEDAD", "DOTRO1", "DOTRO2","ATR_5", "ATR_6",
+     "DNOMBRE", "DMARCA", "DVARIEDAD", "DOTRO1", "DOTRO2", "ATR_5", "ATR_6",
      "SAJU_ITEM", "AJU_ITEM", "CANT_MERC", "MERMAS", "MEDIDA", "PRE_UNIT",
      "ARANC_ALA", "NUMCOR", "NUMACU", "CODOBS1", "DESOBS1", "CODOBS2",
-     "DESOBS2", "CODOBS3","DESOBS3", "CODOBS4", "DESOBS4", "ARANC_NAC",
+     "DESOBS2", "CODOBS3", "DESOBS3", "CODOBS4", "DESOBS4", "ARANC_NAC",
      "CIF_ITEM", "ADVAL_ALA", "ADVAL", "VALAD", "OTRO1", "CTA1", "SIGVAL1",
      "VAL1", "OTRO2", "CTA2", "SIGVAL2", "VAL2", "OTRO3", "CTA3", "SIGVAL3",
-     "VAL3", "OTRO4", "CTA4", "SIGVAL4","VAL4"
-     ]
+     "VAL3", "OTRO4", "CTA4", "SIGVAL4", "VAL4"]
 
 # Year and month to filter
 file_to_filter = {
-    "2017": ["Enero", "Febrero", "Marzo", "Abril", "Mayo"]
+    "2017": ["01 - Enero"]
 }
+
+# Working directory
+cwd = os.getcwd()
 
 # Create filter folder
 if not os.path.exists(cwd + os.sep + "Filter_Data"):
@@ -50,13 +50,15 @@ if not os.path.exists(cwd + os.sep + "Filter_Data"):
 if not os.path.exists(cwd + os.sep + "Filter_Data" + os.sep + "import"):
     os.makedirs(cwd + os.sep + "Filter_Data" + os.sep + "import")
 
+chunksize = 10 ** 3
+
 # Import data
 read_path = cwd + os.sep + "Original_Data" + os.sep + "import"
 write_path = cwd + os.sep + "Filter_Data" + os.sep + "import"
 
 for year, files in file_to_filter.items():
-    for i in range(len(files)):
-        archive = "{} - {}.txt".format(str(i+1).zfill(2), files[i])
+    for file in files:
+        archive = file + ".txt"
         filename = read_path + os.sep + year + os.sep + archive
         filename_out = write_path + os.sep + year + os.sep + archive
 
@@ -64,9 +66,9 @@ for year, files in file_to_filter.items():
             os.makedirs(write_path + os.sep + year)
 
         with open(filename_out, "w") as new_file:
-            print(filename)
-            for chunk in pandas.read_csv(filename, chunksize=chunksize,
-                                         names=columns, header=None,
-                                         sep=";", encoding="latin1"):
-                filter_file(chunk, new_file)
-                break
+            for chunk in pd.read_csv(filename, chunksize=chunksize,
+                                     names=columns, header=None,
+                                     sep=";", encoding="latin1"):
+                filter_import_file(chunk, new_file)
+
+print("Import filtered")
